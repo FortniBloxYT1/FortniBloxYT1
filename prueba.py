f7a9c7a15df0ddc77ef840ab70d51e0fe9c4ec64
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2022-02-09 19:27:31 +0100

    Delete prueba.py

--- prueba.py
+++
@@ -1,6 +0,0 @@
-from base64 import encode
-from selenium import webdriver
-PATH = "C:\Users\aitor\Downloads\chromedriver_win32"
-driver = webdriver.Chrome(PATH)
-driver.get("https://google.com")
-driver.quit()
